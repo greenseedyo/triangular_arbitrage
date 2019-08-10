656d187003e3b9c0357781d9dc94da743cdf35ee
yo <turtleegg@gmail.com>
2019-08-11 01:37:03 +0800

    update liquid.py

--- bot/adapters/liquid.py
+++ bot/adapters/liquid.py
@@ -18,10 +18,5 @@ class LiquidAdapter:
     def get_min_trade_volume_limit(symbol):
         return 0
 
-    def fetch_order_book(self, symbol, limit):
-        if limit < 5:
-            limit = 5
-        return self.client.fetch_order_book(symbol, limit)
-
     def __getattr__(self, name):
         return getattr(self.client, name)
